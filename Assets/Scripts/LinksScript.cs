5008d0c5087f6948e1dd3ba360cb616e37956e76
BenNorthrop1 <90899118+BenNorthrop1@users.noreply.github.com>
2022-02-20 15:34:43 +0000

    update

---
+++ Assets/Scripts/LinksScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinksScript : MonoBehaviour
+{
+  
+
+    public void KnightLink()
+    {
+        Application.OpenURL("https://sketchfab.com/3d-models/strong-knight-9d912e0fedb44b619fbd2a083bbf174f");
+    }
+
+    public void CastleLink()
+    {
+        Application.OpenURL("https://sketchfab.com/3d-models/buckstone-castle-234b87842c2f4d46aea303c89a8dd2b7#download");
+    }
+
+    public void MusicLink()
+    {
+        Application.OpenURL("https://www.serpentsoundstudios.com/royalty-free-music/celtic-fantasy");
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/Settingmenu.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class Settingmenu : MonoBehaviour
+{
+
+    public AudioClip[] clips;
+    public AudioSource vfx;
+    public AudioSource music;
+    public AudioSource master;
+
+    public AudioMixer audioMixer;
+
+
+    private float pMasterVolume, pVfxVolume, pMusicVolume;
+
+    
+
+    public Slider masterSlider;
+
+    public Slider vfxSlider;
+
+    public Slider musicSlider;
+    
+   
+    public TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        pMasterVolume = PlayerPrefs.GetFloat("playerMasterVolume",0);
+
+        pMusicVolume = PlayerPrefs.GetFloat("playerMusicVolume",0);
+
+        pVfxVolume = PlayerPrefs.GetFloat("playerVfxVolume",0);
+
+        audioMixer.SetFloat("MasterVol",pMasterVolume);
+
+        audioMixer.SetFloat("VfxVol",pVfxVolume);
+
+        audioMixer.SetFloat("MusicVol",pMusicVolume);
+
+        resolutions = Screen.resolutions;
+
+        masterSlider.value = pMasterVolume;
+
+        vfxSlider.value = pVfxVolume;
+
+        musicSlider.value = pMusicVolume;
+
+        music.PlayOneShot(clips[0],0.7f);
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+    }
+
+  
+
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+
+    
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+        QualitySettings.SetQualityLevel(2);
+  
+    }
+
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+
+    public void SetVolume(float volume) 
+    {
+        PlayerPrefs.SetFloat("playerMasterVolume", volume);
+        audioMixer.SetFloat("MasterVolume",volume);
+    }
+
+  public void PlaySound(int clipNumber, int output) 
+{
+    switch(output)
+     {
+        
+        case 0:
+              music.PlayOneShot(clips[clipNumber]);
+              break;
+        case 1:
+              vfx.PlayOneShot(clips[clipNumber]);
+              break;
+    }
+}
+}
